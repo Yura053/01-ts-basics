18e5bad5a42f36f9afb52be8c0fd13fb3bac708d
Yurii Ivanes <ivanecura82@gmail.com>
2026-06-07 02:24:05 +0300

    fix04

--- src/task-1.ts
+++ src/task-1.ts
@@ -2,6 +2,6 @@ const userName: string = "Alice";
 const age: number = 30;
 const isOnline: boolean = true;
 
-console.log(`Name: ${name}`);
+console.log(`Name: ${userName}`);
 console.log(`Age: ${age}`);
 console.log(`Online: ${isOnline}`);
